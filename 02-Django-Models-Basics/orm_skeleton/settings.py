0d2200e2b8a57a509d716d45725e9e2fe9e4291f
Todor-Gatev <aleko_stastlivetsa@yahoo.com>
2024-02-17 09:02:49 +0200

    Migration and Admin

--- 02-Django-Models-Basics/orm_skeleton/settings.py
+++ 02-Django-Models-Basics/orm_skeleton/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = 'orm_skeleton.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'models_db',
+        'USER': 'postgres',
+        'PASSWORD': 'aaa',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
