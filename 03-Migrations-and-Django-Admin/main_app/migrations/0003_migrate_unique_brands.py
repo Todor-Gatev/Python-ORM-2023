f112a23c29e2cf9beea0f0a7607311a5b51ebec8
Todor-Gatev <aleko_stastlivetsa@yahoo.com>
2024-02-20 17:42:34 +0200

    Migration and Admin

--- 03-Migrations-and-Django-Admin/main_app/migrations/0003_migrate_unique_brands.py
+++ 03-Migrations-and-Django-Admin/main_app/migrations/0003_migrate_unique_brands.py
@@ -2,17 +2,31 @@
 
 from django.db import migrations
 
+
 def create_unique_brands(apps, schema_editor):
     shoe = apps.get_model('main_app', 'Shoe')
     unique_brands = apps.get_model('main_app', 'UniqueBrands')
 
+    unique_brand_names = shoe.objects.values_list('name', flat=True).distinct()
+    unique_brands_to_create = [unique_brands(brand_name=brand_name) for brand_name in unique_brand_names]
+    unique_brands.objects.bulk_create(unique_brands_to_create)
+
+    # for brand_name in unique_brand_names:
+    #     unique_brands.create(brand_name=brand_name)
+
+
+def delete_unique_brands(apps, schema_editor):
+    unique_brands = apps.get_model('main_app', 'UniqueBrands')
+
+    unique_brands.objects.all().delete()  # same as truncate
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('main_app', '0002_uniquebrands'),
     ]
 
     operations = [
+        migrations.RunPython(create_unique_brands, reverse_code=delete_unique_brands)
+        # Not RunPython(create_unique_brands())
     ]
