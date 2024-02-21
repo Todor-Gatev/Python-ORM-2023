d2a52a719a5e85eca7df09524892b95783894fce
Todor-Gatev <aleko_stastlivetsa@yahoo.com>
2024-02-21 21:50:59 +0200

    Migration and Admin

--- 03-Migrations-and-Django-Admin/main_app/models.py
+++ 03-Migrations-and-Django-Admin/main_app/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 
+# sqlmigrate main_app 0001_initial
 class Shoe(models.Model):
     brand = models.CharField(max_length=25)
     size = models.PositiveIntegerField()
 
 
+# makemigrations main_app --name migrate_unique_brands --empty
 class UniqueBrands(models.Model):
     brand_name = models.CharField(max_length=25)
