22f30f80da25dff81af0e0d70cb4bff50e9f76e9
Todor-Gatev <aleko_stastlivetsa@yahoo.com>
2024-02-23 06:33:25 +0200

    Migration and Admin

--- 03-Migrations-and-Django-Admin/main_app/models.py
+++ 03-Migrations-and-Django-Admin/main_app/models.py
@@ -3,7 +3,9 @@ from django.db import models
 
 # sqlmigrate main_app 0001_initial
 class Shoe(models.Model):
-    brand = models.CharField(max_length=25)
+    brand = models.CharField(
+        max_length=25)
+
     size = models.PositiveIntegerField()
 
 
